50273c50ca2b861006353cc279cba5f40fc59c35
ayushgit12 <ayush2005raj@gmail.com>
2025-02-24 18:11:16 +0530

    init

---
+++ frontend/src/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ShuffleText from 'react-shuffle-text'
+
+const LandingPage = () => {
+  return (
+    <div className='bg-black h-screen flex justify-center items-center'>
+     <div style={{
+          fontFamily: "orbitron",
+          fontWeight: "bold",
+     }}
+     className='text-9xl text-center text-white'
+     >
+
+      <ShuffleText
+       content="HACK/<FEST'25" />
+       </div>
+    </div>
+  )
+}
+
+export default LandingPage
